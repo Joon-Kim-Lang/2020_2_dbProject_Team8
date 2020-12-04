177bc627f5fb8f5317babaf52c8f9a85442cc4aa
jh0shin <59114042+jh0shin@users.noreply.github.com>
2020-12-04 19:23:35 +0900

    Update settings.py

--- dbProject/settings.py
+++ dbProject/settings.py
@@ -58,7 +58,6 @@ CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'dbProject.urls'
 
-SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
 SESSION_SAVE_EVERY_REQUEST = True
 
 TEMPLATES = [
