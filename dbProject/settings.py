f82e20322f8ff1acdc8ad38f5c6d4ed0288fe4db
Pyossyoung <pyow14198@gmail.com>
2020-12-05 20:42:21 +0900

    no more port for db

--- dbProject/settings.py
+++ dbProject/settings.py
@@ -88,7 +88,6 @@ DATABASES = {
         'USER': 'team8',	# 사용자 이름
         'PASSWORD': 'team8',	# 사용자 비밀번호
         'HOST': '165.132.105.42',	# 인스턴스 주소(IP), RDS에서의 엔드포인트
-        'PORT': '3031',	# 포트번호
         'OPTIONS': {
             'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'
         }
